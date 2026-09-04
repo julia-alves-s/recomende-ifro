fa204213048cd211e6d1219f8d8794d29cf0dbc0
julia_alves <juliaalves0725@outlook.com>
2026-09-04 19:35:00 -0400

    base telas principais

---
+++ app/livros/page.tsx
@@ -0,0 +1,15 @@
+import BarraLateral from "@/components/BarraLateral";
+import Header from "@/components/Header";
+import Layout from "@/components/Layout";
+
+export default function testPage() {
+    return (
+        <>
+            <Layout pagina="Sessão de Livros">
+                <div>
+
+                </div>
+            </Layout>
+        </>
+    )
+}
--- components/BarraLateral.tsx
+++ components/BarraLateral.tsx
@@ -11,30 +11,30 @@ export default function BarraLateral() {
                 </div>
                 <div className="border-t m-5">
                     <div className="flex flex-col pt-5 gap-2">
-                        <Link href={`/`} className="flex items-center border p-2">
+                        <Link href={`/filmes`} className="flex items-center border p-2">
                             <p className="ml-2">FILMES</p>
                         </Link>
-                        <Link href={`/`} className="flex items-center border p-2">
+                        <Link href={`/jogos`} className="flex items-center border p-2">
                             <p className="ml-2">JOGOS</p>
                         </Link>
-                        <Link href={`/`} className="flex items-center border p-2">
+                        <Link href={`/series`} className="flex items-center border p-2">
                             <p className="ml-2">SÉRIES</p>
                         </Link>
-                        <Link href={`/`} className="flex items-center border p-2">
+                        <Link href={`/animacoes`} className="flex items-center border p-2">
                             <p className="ml-2">ANIMAÇÕES</p>
                         </Link>
-                        <Link href={`/`} className="flex items-center border p-2">
+                        <Link href={`/livros`} className="flex items-center border p-2">
                             <p className="ml-2">LIVROS</p>
                         </Link>
-                        <Link href={`/`} className="flex items-center border p-2">
+                        <Link href={`/manga-hq`} className="flex items-center border p-2">
                             <p className="ml-2">MANGÁ / HQ</p>
                         </Link>
                     </div>
                 </div>
                 <div className="border-t m-5">
-                    <Link href={`/`} className="flex items-center border p-2 mt-5">
+                    <button className="flex items-center border p-2 mt-5 w-full" id="criar-post">
                         <p className="ml-2">CRIAR POSTAGEM</p>
-                    </Link>
+                    </button>
                 </div>
             </div>
         </div>

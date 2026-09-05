c801257d16f30364f6f7ab3e6dc96879fdccff36
Evelyn do Vale <evelynespildora@gmail.com>
2026-09-04 22:15:46 -0400

    começando

---
+++ components/Card.tsx
@@ -0,0 +1,17 @@
+
+interface novoCard {
+    id: number
+    titulo: string
+    genero: string
+    imagemUrl: string
+    sinopse: string
+    resenha: string
+}
+
+export default function card({titulo, genero, imagemUrl, sinopse, resenha}: novoCard) {
+    return (
+        <>
+
+        </>
+    )
+}

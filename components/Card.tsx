809f271a5625233218a4fdbb85afc05604a8cec3
Evelyn <evelynespildora@gmail.com>
2026-09-10 00:57:53 -0400

    detalher menores de css

--- components/Card.tsx
+++ components/Card.tsx
@@ -16,6 +16,7 @@ export default function Card({
     imagemUrl,
     sinopse,
     resenha,
+    recomendacao,
 }: CardProps) {
     return (
         <div className="w-full max-w-md border overflow-hidden border-8 border-[#606060] rounded-xl bg-[#606060]">
@@ -40,9 +41,24 @@ export default function Card({
                         className="w-full h-full object-cover"
                     />
                 </div>
-                <p className="text-sm text-black/80 leading-snug">
-                    {sinopse}
-                </p>
+
+                <div className="flex flex-col justify-between flex-1">
+                    <p className="text-sm text-black/80 leading-snug">
+                        {sinopse}
+                    </p>
+
+                    <div className="flex justify-end mt-2">
+                        {recomendacao ? (
+                            <span className="bg-green-500 rounded-xl text-xs border border-green-600 px-2 py-1 text-white">
+                                RECOMENDADO
+                            </span>
+                        ) : (
+                            <span className="bg-red-500 rounded-xl text-xs border border-red-600 px-2 py-1 text-white">
+                                NÃO RECOMENDADO
+                            </span>
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
     );
--- components/ModalPost.tsx
+++ components/ModalPost.tsx
@@ -24,14 +24,18 @@ export default function ModalPost({ isOpen, onClose, children }: ModalProps) {
             className="fixed inset-0 bg-black/60 flex items-center justify-center z-50"
             onClick={onClose}>
             <div
-                className="bg-[#2e2e2e] text-white/80 w-full max-w-lg max-h-[90vh] overflow-y-auto p-6 relative rounded-xl"
+                className="bg-[#2e2e2e] text-white/80 w-full max-w-lg max-h-[90vh] rounded-xl overflow-hidden relative"
                 onClick={(e) => e.stopPropagation()}>
                 <button
                     onClick={onClose}
-                    className="absolute top-3 right-3 text-white/60 hover:text-white"
-                    aria-label="Fechar"
-                    > ✕ </button>
-                {children}
+                    className="absolute top-3 right-3 text-white/60 hover:text-white z-10"
+                    aria-label="Fechar">
+                    ✕
+                </button>
+
+                <div className="max-h-[90vh] overflow-y-auto p-6 scrollbar-custom">
+                    {children}
+                </div>
             </div>
         </div>
     );
